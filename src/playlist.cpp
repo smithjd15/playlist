126f44e4a18d0c13cd5e085149c0252cfa781fa0
James D. Smith <smithjd15@gmail.com>
2023-01-04 11:52:52 -0700

    Bump version.

--- src/playlist.cpp
+++ src/playlist.cpp
@@ -52,7 +52,7 @@ namespace fs = std::filesystem;
 
 Flags flags;
 std::stringstream cwar;
-std::string ver = "2.6";
+std::string ver = "2.7";
 
 void show(const List &list) {
   time_t totalDuration(0);
